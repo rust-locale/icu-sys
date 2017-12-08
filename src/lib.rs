db0cfc0abf79a52741275c9e5ffbd29cdbff0010
Jan Hudec <bulb@ucw.cz>
2017-12-08 23:47:22 +0100

    First link test.
    Found two things:
    
    1. Bindgen extracts the real, versioned, names. We'll have to create the
       aliases ourselves.
    2. Working with CStr is pain, but it's nothing compared to working with
       output [c_char] buffers. It will need a handful of helpers somewhere.

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,24 @@ include!(concat!(env!("OUT_DIR"), "/icu.rs"));
 
 #[cfg(test)]
 mod tests {
+    use std::ffi::CStr;
+    use std::os::raw::c_char;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn uloc_linked() {
+        let loc = unsafe { CStr::from_bytes_with_nul_unchecked(b"en-IN\0") };
+        let mut buf = [0 as u8; 12];
+        let mut err = super::UErrorCode_U_ZERO_ERROR;
+        unsafe {
+            let len = super::uloc_getLanguage_57(loc.as_ptr(), &mut buf as *mut _ as *mut c_char, buf.len() as i32, &mut err as *mut _) as usize;
+            assert_eq!("en".as_bytes(), &buf[0..len]);
+        }
+        unsafe {
+            let len = super::uloc_getScript_57(loc.as_ptr(), &mut buf as *mut _ as *mut c_char, buf.len() as i32, &mut err as *mut _) as usize;
+            assert_eq!("".as_bytes(), &buf[0..len]);
+        }
+        unsafe {
+            let len = super::uloc_getCountry_57(loc.as_ptr(), &mut buf as *mut _ as *mut c_char, buf.len() as i32, &mut err as *mut _) as usize;
+            assert_eq!("IN".as_bytes(), &buf[0..len]);
+        }
     }
 }
